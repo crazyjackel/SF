7e86ff85c35ab8cd071462ab74a28d702735c213
JamesOrrRIT <54995791+JamesOrrRIT@users.noreply.github.com>
2022-03-03 20:37:26 -0500

    Levels updated
    The first three levels are now recreated from the ones last Friday, along with two new levels. Each was designed to ease players into the mechanics slowly with the tutorial backgrounds explaining the elements

--- Assets/Scripts/UI/LevelSelectView.cs
+++ Assets/Scripts/UI/LevelSelectView.cs
@@ -12,9 +12,13 @@ public class LevelSelectView : View<LevelSelectViewModel>
         var level1 = Root.Q<Button>("level1Button");
         var level2 = Root.Q<Button>("level2Button");
         var level3 = Root.Q<Button>("level3Button");
+        var level4 = Root.Q<Button>("level4Button");
+        var level5 = Root.Q<Button>("level5Button");
 
         level1.BindCallback(viewModel.OnClick, "level1").AddTo(disposable);
         level2.BindCallback(viewModel.OnClick, "level2").AddTo(disposable);
         level3.BindCallback(viewModel.OnClick, "level3").AddTo(disposable);
+        level4.BindCallback(viewModel.OnClick, "level4").AddTo(disposable);
+        level5.BindCallback(viewModel.OnClick, "level5").AddTo(disposable);
     }
 }
