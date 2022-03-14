e11cf1f3a3524e61263485ea00e64883c2a49b68
James Orr (RIT Student) <jpo3042@rit.edu>
2022-03-14 13:48:00 -0400

    Slight Changes 3/14
    -Added the Blue and Yellow tile colors
    -Moved the original level 5 to be level 10
    Created new level 5, as well as levels 6 and 8

--- Assets/Scripts/UI/LevelSelectView.cs
+++ Assets/Scripts/UI/LevelSelectView.cs
@@ -14,11 +14,17 @@ public class LevelSelectView : View<LevelSelectViewModel>
         var level3 = Root.Q<Button>("level3Button");
         var level4 = Root.Q<Button>("level4Button");
         var level5 = Root.Q<Button>("level5Button");
+        var level6 = Root.Q<Button>("level6Button");
+        var level8 = Root.Q<Button>("level8Button");
+        var level10 = Root.Q<Button>("level10Button");
 
         level1.BindCallback(viewModel.OnClick, "level1").AddTo(disposable);
         level2.BindCallback(viewModel.OnClick, "level2").AddTo(disposable);
         level3.BindCallback(viewModel.OnClick, "level3").AddTo(disposable);
         level4.BindCallback(viewModel.OnClick, "level4").AddTo(disposable);
         level5.BindCallback(viewModel.OnClick, "level5").AddTo(disposable);
+        level6.BindCallback(viewModel.OnClick, "level6").AddTo(disposable);
+        level8.BindCallback(viewModel.OnClick, "level8").AddTo(disposable);
+        level10.BindCallback(viewModel.OnClick, "level10").AddTo(disposable);
     }
 }
