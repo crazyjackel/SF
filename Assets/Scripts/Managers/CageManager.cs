55a3e04bdeb3e2af8216c29f95a58f262b7c6dea
KeeganL14 <55001195+KeeganL14@users.noreply.github.com>
2022-04-27 21:39:15 -0400

    Meerkat (#44)
    * Quick Update
    
    * MeerKat
    
    Added the animals to their respective worlds and animated the meerkat
    
    * Animal Positions
    
    Added slight randomness to the animal's positions once they're free from the cages
    
    Co-authored-by: Jackson Levitt <47047592+crazyjackel@users.noreply.github.com>

--- Assets/Scripts/Managers/CageManager.cs
+++ Assets/Scripts/Managers/CageManager.cs
@@ -27,7 +27,15 @@ public class CageManager : BaseManager
             if (m_save.IsLevelComplete(levelName))
             {
                 cage.SpriteRenderer.sprite = CageCompleteSprite;
-                if(spawnPrefab) Instantiate(spawnPrefab, cage.transform);
+                if (spawnPrefab)
+                {
+                    //randomize the animal's spawn point around the cage
+                    GameObject animal = Instantiate(spawnPrefab, cage.gameObject.transform);
+                    animal.transform.position = new Vector3(
+                        Random.Range(cage.gameObject.transform.position.x - 1f, cage.gameObject.transform.position.x + 1f),
+                        Random.Range(cage.gameObject.transform.position.y - 1f, cage.gameObject.transform.position.y + 1f),
+                        0f);
+                }
             }
             else if (cage.PreviousLevelName != "" && !m_save.IsLevelComplete(cage.PreviousLevelName))
             {
